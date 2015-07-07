aea9e5f9ab7cf7d6e013ac1b594ef60bd6af3a14
user <user@test.com>
2015-07-07 11:01:05 +0900

    t

---
+++ work01/class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+print("1")
+class hoge():
+    print("2");
+    def __init__(self):
+        print("3")
+        return;
+    def test1():
+        print("4")
+        return;
+
+
+test = hoge()
+
---
+++ work01/test.py
@@ -0,0 +1,36 @@
+'''
+ハローワールド！
+'''
+print("hello world!!")
+
+
+'''
+分岐の練習！
+'''
+vipper = "neko"
+if vipper == "hage":
+    print("hage yeah!")
+elif vipper == "neko":
+    print("nyanya")
+else:
+    print("not hage....")
+
+
+'''
+繰り返しの練習forバージョン
+'''
+
+for i in range(0,30):
+    print(i);
+print("End")
+
+
+'''
+whileもやるよ！
+'''
+
+j = 0
+while j<10:
+    j+=1
+    print(j);
+print("End");
