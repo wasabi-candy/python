d9305eb90703d44a41db6c51b2cb95e5b400dfdf
user <user@test.com>
2015-07-07 13:13:39 +0900

    t

--- work02/window.py
+++ work02/window.py
@@ -1,14 +1,19 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import tkinter as test
+import tkinter as tk
 
 class Frame(tk.Frame):
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
         self.pack()
+        self.setCanvas();
+    def setCanvas(self):
+        canvas = tk.Canvas(self,width=700,height=500,bg="#aaa");
+        canvas.create_rectangle(0,0,700,500,width=0)
+        rect = canvas.create_rectangle(5,5,30,30,width=1,fill="#3f7")
+        canvas.pack()
+
 
 f = Frame()
 f.mainloop()
-
-
