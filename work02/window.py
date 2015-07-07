867b5af45817e94cd1ebab11677cded23f28c3ff
user <user@test.com>
2015-07-07 13:52:45 +0900

    t

--- work02/window.py
+++ work02/window.py
@@ -8,11 +8,16 @@ class Frame(tk.Frame):
         tk.Frame.__init__(self, master)
         self.pack()
         self.setCanvas();
+        self.rectx = 5;
+        self.recty = 5;
     def setCanvas(self):
         canvas = tk.Canvas(self,width=700,height=500,bg="#aaa");
         canvas.create_rectangle(0,0,700,500,width=0)
-        rect = canvas.create_rectangle(5,5,30,30,width=1,fill="#3f7")
+        rect = canvas.create_rectangle(self.rectx,point.recty,30,30,width=1,fill="#3f7")
         canvas.pack()
+        self.moveRect()
+    def moveRect(self):
+        print("test");
 
 
 f = Frame()
